940006a90017b01b8250b5f817365c79879c170c
Peter Sobhy <PSobhy@integrant.com>
2017-10-11 17:20:30 +0200

    add anfular reactive form attrs

--- Client/Angular4Client/src/app/evaluation/candidate-info-form/candidate-info-form.component.ts
+++ Client/Angular4Client/src/app/evaluation/candidate-info-form/candidate-info-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
 import { IPoistion } from './../model/position.model';
@@ -11,11 +11,13 @@ import { KpiService } from './../kpi.service';
   styleUrls: ['./candidate-info-form.component.css']
 })
 export class CandidateInfoFormComponent implements OnInit {
+  candidateInfo: FormGroup;
+
   positionCtrl: FormControl;
   filteredPositions$: Observable<IPoistion[]>;
   positions: IPoistion[] = [];
 
-  constructor(private dataService: KpiService) {
+  constructor(private dataService: KpiService, private fb: FormBuilder) {
 
     this.positionCtrl = new FormControl();
 
@@ -33,6 +35,11 @@ export class CandidateInfoFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.candidateInfo = this.fb.group({
+      name: ['candidate_name', Validators.required],
+      position: 'candidate_position'
+    });
+
     this.dataService.getWorkPositions().subscribe(positions => this.positions = positions);
   }
 
